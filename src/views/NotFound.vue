07ec5f52beacf76d04c5ccae280133644fa0ad0a
María Jesús Fuenzalida <maria.fuenzalida.serrano@gmail.com>
2020-07-18 13:40:12 -0400

    Avance 7 con NOTFOUND listo

---
+++ src/views/NotFound.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="not__found">
+    <img class="error__404" src="error-404.png" alt="error-404">
+    <div class="text-center btn__nf">
+      <v-btn rounded color="grey" to="/login">🌟 Volver a Login 🌟</v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.not__found {
+  background-color: #000;
+}
+.error__404 {
+  width: 30em;
+  display: block;
+  margin: auto;
+  margin-top: 3em;
+}
+.btn__nf {
+  margin-bottom: 8.5em;
+}
+</style>
