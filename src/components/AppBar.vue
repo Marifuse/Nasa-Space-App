55098eb677c14885c20c2e7eef44716808f3862f
María Jesús Fuenzalida <maria.fuenzalida.serrano@gmail.com>
2020-07-18 10:58:05 -0400

    Avance 1 con incorporación de vistas y algunos componentes

---
+++ src/components/AppBar.vue
@@ -0,0 +1,102 @@
+<template>
+  <div>
+    <v-app-bar
+      app
+      color="grey darken-3"
+      dark
+    >
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <a href="home" class="nav__a"><v-toolbar-title>»Nasa~Space«</v-toolbar-title></a>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+        >
+
+          <v-list-item>
+            <v-btn
+              text
+              @click="logout"
+            >
+              <span class="mr-1">{{ !!currentUser ? 'Cerrar Sesión' : 'Iniciar Sesión' }}</span>
+              <v-icon>mdi-lock</v-icon>
+            </v-btn>
+          </v-list-item>
+
+          <v-list-item>
+            <v-btn
+              text
+              to="/admin"
+            >
+              <span class="mr-1">Apod</span>
+              <v-icon>mdi-chart-bubble</v-icon> 
+            </v-btn>
+          </v-list-item>
+
+          <v-list-item>
+            <v-btn
+              text
+              to="/admin"
+            >
+              <span class="mr-1">Rover</span>
+              <v-icon>mdi-star</v-icon> 
+            </v-btn>
+          </v-list-item>
+
+          <v-list-item>
+            <v-btn
+              text
+              to="/home"
+            >
+              <span class="mr-1">Home</span>
+              <v-icon>mdi-home</v-icon>
+            </v-btn>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+import Firebase from 'firebase'
+import { mapState, mapActions } from 'vuex'
+export default {
+  data: () => ({
+    drawer: false,
+  }),
+  methods: {
+    ...mapActions(['setCurrentUser']),
+    logout() {
+      Firebase.auth().signOut().then(() => {
+        this.setCurrentUser(this.user)
+        this.$router.push('/login')
+      })
+    }
+  },
+  computed: {
+    ...mapState(['currentUser'])
+  }  
+}
+</script>
+
+<style>
+.nav__a {
+  text-decoration: none;
+  color: #FFF !important;
+  transition: .3s;
+}
+.nav__a:hover {
+  color: rgb(185, 185, 185) !important;
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Firebase from 'firebase'
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
@@ -7,15 +8,42 @@ Vue.use(VueRouter)
   const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: Home, // El component llama al componente importado
+    meta: {
+      requireLogin: true // El meta tiene relación con la función guardia (se representa con una respuesta booleana)
+    }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    alias: ['/autenticacion', '/usuario'],
+    component: () => import(/* webpackChunkName: "Login" */ '../views/Login.vue')
+  },
+  {
+    path: '/apod',
+    name: 'Apod',
+    component: () => import(/* webpackChunkName: "Apod" */ '../views/Apod.vue'),
+    alias: ['/busqueda', '/buscar'],
+  },
+  {
+    path: '/rover',
+    name: 'Rover',
+    component: () => import(/* webpackChunkName: "Rover" */ '../views/Rover.vue'),
+    alias: ['/busqueda', '/buscar'],
+  },
+  {
+    path: '*', // Este tipo de path con asterisco se usa para generar un not found o error 404
+    name: 'NotFound',
+    component: () => import(/* webpackChunkName:"notfound" */ '../views/NotFound') //Jamas debe ir con espacios
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
@@ -24,6 +52,16 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  let user = Firebase.auth().currentUser;
+  let authRequired = to.matched.some(route => route.meta.requireLogin)
+  if(!user && authRequired) {
+    next('login') 
+  } else {
+    next()
+  }
 })
 
 export default router
