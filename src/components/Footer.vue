d24f076037ca22c1c66cc320bd6be2b06adac0e0
María Jesús Fuenzalida <maria.fuenzalida.serrano@gmail.com>
2020-07-18 12:17:10 -0400

    Avance 4 con Footer y Home que se pueden ver (incorporación de carousel)

---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-footer padless color="grey darken-4" dark>
+    <v-col
+      class="text-center"
+      cols="12"
+    >
+      {{ new Date().getFullYear() }} — <strong>🚀Nasa~Space✨ <br> Todos los Derechos Reservados®</strong>
+    </v-col>
+  </v-footer>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,1 +1,64 @@
+<template>
+  <div class="home home__container">
+      <v-carousel
+        continuous
+        cycle
+        class="pt-2"
+      >
+        <v-carousel-item
+          v-for="(item,i) in items"
+          :key="i"
+          :src="item.src"
+          reverse-transition="fade-transition"
+          transition="fade-transition"
+        ></v-carousel-item>
+      </v-carousel>
+      <header class="text-center header__container">
+        <h1>⭐Nasa~Space⭐</h1>
+        <h2>¿Quieres conocer el Infinito y más Allá?</h2>
+        <h4>Ven y descúbrelo</h4>
+      </header>
+    <Footer/>
+  </div>
+</template>
 
+<script>
+import Footer from '../components/Footer'
+
+export default {
+  data () {
+    return {
+      items: [
+        {
+          src: 'http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG',
+        },
+        {
+          src: 'https://apod.nasa.gov/apod/image/2007/NEOWISEBelowBigDipper-7-16-2020-TomMasterson1081.jpg',
+        },
+        {
+          src: 'https://api.nasa.gov/EPIC/archive/natural/2019/05/30/png/epic_1b_20190530011359.png?api_key=DEMO_KEY',
+        },
+        {
+          src: 'https://www.nasa.gov/sites/default/files/thumbnails/image/curiosity_selfie.jpg',
+        },
+        {
+          src: 'https://eoimages.gsfc.nasa.gov/images/imagerecords/146000/146996/iss063e039892_lrg.jpg',
+        }
+      ],
+    }
+  },
+  name: 'Home',
+  components: {
+    Footer
+  },
+}
+</script>
+
+<style lang="scss">
+.home__container {
+  background-color: gainsboro;
+}
+.header__container {
+  background-color: #FFF;
+} 
+</style>
