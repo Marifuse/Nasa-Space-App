c817948220bbe9321219fa88bf7a65c38f777742
María Jesús Fuenzalida <maria.fuenzalida.serrano@gmail.com>
2020-07-18 21:30:39 -0400

    Avance 13 con arreglos adicionales en las vistas Apod y Rover

--- src/components/RoverForm.vue
+++ src/components/RoverForm.vue
@@ -1,34 +1,32 @@
 <template>
   <div class="rover">
-    <main class="rover__container mt-10 grey lighten-2">
-      <v-container>
-        <h2>Busqueda de Imagenes</h2>
-        <v-text-field label='Sol' 
-          color="dark"
-          />
-        <v-row align="center">
-        <v-subheader>Rover</v-subheader>
-          <v-select
-            v-model="select"
-            :hint="`${select.item}`"
-            :items="items"
-            item-text="item"
-            label="Select"
-            persistent-hint
-            return-object
-            single-line
-            color="dark"
-          ></v-select>
-        </v-row>
-        <div class="mt-10 text-center">
-          <v-btn color='black' dark @click="search">🪐 Buscar 🪐</v-btn>
-        </div>
-      </v-container>
-    </main>
-    <div class="text-center pa-15">
-      <v-btn class="mx-2" color="black" dark to="/home">🚀 Volver a Home 🚀</v-btn>
-    </div>
-  </div>
+    <section class="text-center">
+      <v-card class="pa-5 grey lighten-3 mb-15 mx-auto" width="30em">
+        <h1 class="text-center mb-5">Busqueda de Imagenes</h1>
+          <v-text-field label="Sol" type="text" outlined color="grey lighten-1"/>
+          <v-row align="center">
+            <v-subheader>Rover</v-subheader>
+              <v-select
+                v-model="select"
+                :hint="`${select.item}`"
+                :items="items"
+                item-text="item"
+                label="Select"
+                persistent-hint
+                return-object
+                single-line
+                color="dark"
+              ></v-select>
+          </v-row>
+          <div class="text-center pa-3">
+            <v-btn color='black' dark @click="search">🪐 Buscar 🪐</v-btn>
+          </div> 
+      </v-card>
+      <div class="text-center pa-15">
+        <v-btn color="black" dark to="/home">🚀 Volver a Home 🚀</v-btn>
+      </div>  
+    </section>
+  </div> 
 </template>
 
 <script>
@@ -62,13 +60,7 @@
   margin: 0;
   background-image: url(https://www.nasa.gov/sites/default/files/thumbnails/image/curiosity_selfie.jpg) !important;
   background-size: cover !important;
-  padding: 1em;
-}
-.rover__container {
-  width: 26em;
-  text-align: center;
-  margin: auto;
-  padding: 3em;
-  border-radius: 2em;
+  padding-top: 3em;
+  padding-bottom: 1em;
 }
 </style>
--- src/views/Apod.vue
+++ src/views/Apod.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="text-center apod__container">
-    <v-btn class="mx-2" color="black" dark to="/home">🚀 Volver a Home 🚀</v-btn>
+    <section class="text-center apod__section">
+      <v-btn color="black" dark to="/home">🚀 Volver a Home 🚀</v-btn>
+    </section>
   </div>
 </template>
 
@@ -15,6 +17,9 @@ export default {
   margin: 0;
   background-image: url(https://api.nasa.gov/EPIC/archive/natural/2019/05/30/png/epic_1b_20190530011359.png?api_key=DEMO_KEY) !important;
   background-size: cover !important;
-  padding: 17.5em;
+}
+.apod__section {
+  margin-top: 20em;
+  margin-bottom: 15em;
 }
 </style>
